031658316dbecbd6f810e16a8e3f8ded2fe68635
Donald Davis <donald.m.davis@sherwin.com>
2017-05-16 16:00:16 -0400

    having a problem with bootstrap

---
+++ ClashOfClans/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClashOfClans.Startup))]
+namespace ClashOfClans
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
