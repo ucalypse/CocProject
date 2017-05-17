090a2410f42ab02890b9b3d0bbe750a27d87592e
Donald Davis <donald.m.davis@sherwin.com>
2017-05-17 14:20:21 -0400

    working

---
+++ ClashOfClans/Controllers/TownHallController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClashOfClans.Controllers
+{
+    public class TownHallController : Controller
+    {
+        // GET: TownHall
+        public ActionResult ThreeThrough6()
+        {
+            return View();
+        }
+        public ActionResult TownHall7()
+        {
+            return View();
+        }
+        public ActionResult TownHall8()
+        {
+            return View();
+        }
+        public ActionResult TownHall9()
+        {
+            return View();
+        }
+        public ActionResult TownHall10()
+        {
+            return View();
+        }
+    }
+}
