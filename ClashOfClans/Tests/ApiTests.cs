1f5a805b9e9b7c6b405da244e623e1c5af7bcd0b
Donald Davis <donald.m.davis@sherwin.com>
2017-05-18 14:04:08 -0400

    wah

---
+++ ClashOfClans/Tests/ApiTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClashOfClans.Controllers;
+using NUnit.Framework;
+
+namespace ClashOfClans.Tests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        HomeController homeController = new HomeController();
+        [Test]
+        public void getClans_Returns_Clan()
+        {
+            Assert.That(homeController.GetClanList("8UJGPROJ"), Is.EqualTo("In yo face"));
+        }
+    }
+}
