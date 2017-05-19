9360692e440d0605d679ef14ee09dcf85bae30a1
Donald Davis <donald.m.davis@sherwin.com>
2017-05-19 09:00:12 -0400

    added restsharp

---
+++ ClashOfClans/ApiCalls/Clans.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+
+namespace ClashOfClans.ApiCalls
+{
+    public class Clans
+    {
+        public IEnumerable<Clans> GetClans{
+            var client = new RestClient("http://api.clashofclans.com/v1/");
+
+            var request = new RestRequest("clans", Method.GET);
+            request.AddParameter("name", "in yo face");
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Authorization",
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNkMDkzZThhLWMxNTAtNDI1NS04NTA4LWMzYTBlMTU1MTMyNiIsImlhdCI6MTQ5NTE1MzMyNSwic3ViIjoiZGV2ZWxvcGVyLzEzNmQ0NzI1LTYyMWItMmY1OC1iNTcwLWM1ZGY2ZmVmN2U2MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwOC44NC4xOTguMTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.hIjNh83gOEbkbLdaQn-9u8gkSxFuyOAoDaq1tarxlvpKolZh5kKQYP9MCGhitYSaeJNUuJwIm7kUTXeHXZPYug");
+
+            Console.WriteLine("Sending request....");
+            var response = client.Execute(request);
+            Console.WriteLine("Request Completed....");
+
+            var responseContent = response.Content;
+            Console.WriteLine("Sending request content to file...");
+            var file = new System.IO.StreamWriter("..\\testApiCall.txt");
+            file.WriteLine("This is the response: " + responseContent);
+            file.WriteLine(response.ErrorMessage);
+            file.WriteLine(response.ErrorException);
+            file.Close();
+
+            Console.ReadLine();
+            return new IEnumerable<Clans>();
+    }
+    
+}
--- ClashOfClans/Controllers/HomeController.cs
+++ ClashOfClans/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
@@ -9,7 +7,7 @@ using System.Web.Mvc;
 
 namespace ClashOfClans.Controllers
 {
-    public class HomeController : Controller
+    public partial class HomeController : Controller
     {
         public ActionResult Index()
         {
@@ -28,23 +26,6 @@ namespace ClashOfClans.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public object GetClanList(String clanTag)
-        {
-            //var getter = new HttpClient();
-            //getter.BaseAddress = new Uri("https://api.clashofclans.com/v1/clans");
-            //getter.
-            string URI = "https://api.clashofclans.com/v1/clans";
-            string myParameters = clan;
-            var token =
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNlNDBkMGJlLTVjYzctNGI5ZS04NGNmLTFmZDExNGYwZDhhNyIsImlhdCI6MTQ5NTA1NzYwOSwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjI1NS4yNTUuMjU1LjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.rDge_8Q85hs2mGxyCEfwWu1Er0va4Jsjdud77LxomjUgZ1T-S6K5UmobZyfgvvRta00p9RtJG2pFHc_54fCSLA";
-            using (WebClient wc = new WebClient())
-            {
-                wc.Headers[HttpRequestHeader.ContentType] = "Authorization: Bearer " + token;
-                string htmlResult = wc.UploadString(URI, myParameters);
-                return htmlResult;
-            }
         }
     }
 }
