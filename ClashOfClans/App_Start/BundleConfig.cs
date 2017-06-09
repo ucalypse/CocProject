920be42b7d1c351448bdc953a5f70513f7957d6f
Donald Davis <donald.m.davis@sherwin.com>
2017-06-09 14:47:19 -0400

    nice working version in production

--- ClashOfClans/App_Start/BundleConfig.cs
+++ ClashOfClans/App_Start/BundleConfig.cs
@@ -27,6 +27,9 @@ namespace ClashOfClans
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-{version}.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
