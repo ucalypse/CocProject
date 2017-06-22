7875e260f9ecd32b82ff263f45271903fcd6d4a0
Donald Davis <davis199994@yahoo.com>
2017-06-22 19:07:46 -0400

    got knockout 2 way binding partially working

--- ClashOfClans/Scripts/TownHall/ServerSide.js
+++ ClashOfClans/Scripts/TownHall/ServerSide.js
@@ -1,9 +1,24 @@
 ﻿$(function () {
+    var ClanMember = function() {
+        this.name = ko.observable();
+        this.playerTag;
+        this.townHallLevel;
+    };
+    wahwah = [];
     $.getJSON('/TownHall/GetFilteredList')
-        .done(function(response) {
+        .done(function (response) {
+            $.each(response.Members,
+                function(i,k) {
+                    wahwah.push(new ClanMember()
+                        .name(k.Name)
+                        .playerTag = k.PlayerTag
+                        .townHallLevel = k.TownHallLevel
+                    );
+                });
+            
             ko.applyBindings(response);
         });
-
+    alert(wahwah);
     $("#beginnerTable").hide();
 });
 
