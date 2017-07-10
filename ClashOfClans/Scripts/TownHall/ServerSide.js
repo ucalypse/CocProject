2e61bf5065d4dc51978c4ded1544b6ac8080cf74
Donald Davis <davis199994@yahoo.com>
2017-07-10 11:36:43 -0400

    wah

--- ClashOfClans/Scripts/TownHall/ServerSide.js
+++ ClashOfClans/Scripts/TownHall/ServerSide.js
@@ -1,27 +1,34 @@
 ﻿$(function () {
-    var ClanMember = function() {
-        this.name = ko.observable();
-        this.playerTag;
-        this.townHallLevel;
+    var ClanMember = {
+        name : ko.observable(),
+        playerTag: ko.observable(),
+        townHallLevel: ko.observable()
     };
-    wahwah = [];
+    var wahwah = [];
     $.getJSON('/TownHall/GetFilteredList')
         .done(function (response) {
             $.each(response.Members,
                 function(i,k) {
-                    wahwah.push(new ClanMember()
-                        .name(k.Name)
-                        .playerTag = k.PlayerTag
-                        .townHallLevel = k.TownHallLevel
-                    );
+                    ClanMember.name = k.Name;
+                    ClanMember.playerTag=k.PlayerTag;
+                    ClanMember.townHallLevel=k.TownHallLevel;
+                    wahwah.push(ClanMember);
+                    i++;
                 });
-            
             ko.applyBindings(response);
         });
-    alert(wahwah);
+    
     $("#beginnerTable").hide();
 });
-
+var filterResults = function() {
+    $.each($('#beginnerTable'),
+        function(i, k) {
+            if (k === i) {
+                alert("wah");
+            }
+        });
+    alert("You called filter results");
+};
 var display = (function () {
     $("#beginnerTable").show("slow");
 });
