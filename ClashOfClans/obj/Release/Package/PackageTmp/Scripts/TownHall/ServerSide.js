b2426f395c687fe733adffaf81b29205f2099aa8
Donald Davis <davis199994@yahoo.com>
2017-07-12 20:29:07 -0400

    youtube rework

--- ClashOfClans/obj/Release/Package/PackageTmp/Scripts/TownHall/ServerSide.js
+++ ClashOfClans/obj/Release/Package/PackageTmp/Scripts/TownHall/ServerSide.js
@@ -1,9 +1,23 @@
 ﻿$(function () {
+    var ClanMember = function() {
+        this.name = ko.observable();
+        this.playerTag;
+        this.townHallLevel;
+    };
+    wahwah = [];
     $.getJSON('/TownHall/GetFilteredList')
-        .done(function(response) {
+        .done(function (response) {
+            $.each(response.Members,
+                function(i,k) {
+                    wahwah.push(new ClanMember()
+                        .name(k.Name)
+                        .playerTag = k.PlayerTag
+                        .townHallLevel = k.TownHallLevel
+                    );
+                });
+            
             ko.applyBindings(response);
         });
-
     $("#beginnerTable").hide();
 });
 
