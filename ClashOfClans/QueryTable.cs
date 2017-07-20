d5c303dc6a78cfd4f29d9d2f20cc604c95845027
Donald Davis <donald.m.davis@sherwin.com>
2017-07-20 14:49:06 -0400

    wah

--- ClashOfClans/QueryTable.cs
+++ ClashOfClans/QueryTable.cs
@@ -11,10 +11,18 @@ namespace ClashOfClans
 {
     public class QueryTable
     {
-        CloudTable table = new CloudTable(new Uri("https://apicalls8e64.table.core.windows.net/"), new StorageCredentials("apicalls864", "F9b1FHzrrm + DXWxSUyL / +UHpu / kkIBf1BDCtkDkWpmVv8K1Q2tRHw0gvXjIAYg7kdrnl3UWE6Am8vW81ffCA5g =="));
+        public string storageName = "apicallse64";
+        public string credentials =
+            "F9b1FHzrrm + DXWxSUyL / +UHpu / kkIBf1BDCtkDkWpmVv8K1Q2tRHw0gvXjIAYg7kdrnl3UWE6Am8vW81ffCA5g ==";
+        public string baseUri = "https://apicalls8e64.table.core.windows.net/";
+
+
+        
         public int GetWarStars(string playerTag)
         {
-           
+            CloudTable table = new CloudTable(new Uri(baseUri), new StorageCredentials(storageName, credentials));
+            CloudTableClient client = new CloudTableClient(new Uri(baseUri), new StorageCredentials(storageName, credentials));
+
             return 0;
         }
     }
