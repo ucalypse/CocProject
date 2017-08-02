12772f9366434f0ea11401edf3827ec09268bad0
Donald Davis <donald.m.davis@sherwin.com>
2017-08-02 08:37:14 -0400

    changed to call api directly

--- ClashOfClans/ApiCalls/ApiCall.cs
+++ ClashOfClans/ApiCalls/ApiCall.cs
@@ -6,16 +6,18 @@ namespace ClashOfClans.ApiCalls
 {
     public class ApiCall
     {
-        RestClient azureClient = new RestClient("https://apicalls.azurewebsites.net/api/");
         RestClient client = new RestClient("https://api.clashofclans.com/v1/");
 
         public List<Member> GetOurClan()
         {
-            var azureToken = "ct41QO9aZ8EWhu3yb80gP6a2ggH5wlpL6P0fApCPtBLnxSC4ppq/Nw==";
+            string token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJjYWQxOWEyLTMyNDgtNDE2My04ZjJiLTIzNmUzMjA3NzM1MyIsImlhdCI6MTUwMTU1NzQ1Niwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzLjg0LjE4MS40NyIsIjEzLjY1LjQyLjQyIiwiMTMuNjUuNDIuMzUiXSwidHlwZSI6ImNsaWVudCJ9XX0.tNY-RncRfJrho1AKSDSp6TLdRhBQPapiSZCJFxEOu5o7wFznq7mCQhxEwnYKkoNCkMUY0OYJh8VccHQ_Bteucw";
 
-            var azureRequest = new RestRequest("GetClanInfo?code=CcCrot5q10/R8WKilALr1UANRmI413gRkKqeTbJjAxxy5oh3AeGoIg==", Method.GET);
-            azureRequest.AddHeader("Accept", "application/json");
-            var response = azureClient.Execute<ClanListViewModel>(azureRequest);
+            var request = new RestRequest("clans/{clanTag}/members", Method.GET);
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Authorization", token);
+            request.AddParameter("clanTag", "#8UJGPROJ", ParameterType.UrlSegment);
+            var response = client.Execute<ClanListViewModel>(request);
             return response.Data.Members;
         }
         public List<Member> FilterMembers(List<Member> filterList, int townHallLevel)
