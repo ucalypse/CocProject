592d590f3bc47bbdf82fd5835e4787076b207c0b
Donald Davis <donald.m.davis@sherwin.com>
2017-08-04 15:02:33 -0400

    For real working site again

--- ClashOfClans/ApiCalls/ApiCall.cs
+++ ClashOfClans/ApiCalls/ApiCall.cs
@@ -7,11 +7,10 @@ namespace ClashOfClans.ApiCalls
     public class ApiCall
     {
         RestClient client = new RestClient("https://api.clashofclans.com/v1/");
+        string token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJjYWQxOWEyLTMyNDgtNDE2My04ZjJiLTIzNmUzMjA3NzM1MyIsImlhdCI6MTUwMTU1NzQ1Niwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzLjg0LjE4MS40NyIsIjEzLjY1LjQyLjQyIiwiMTMuNjUuNDIuMzUiXSwidHlwZSI6ImNsaWVudCJ9XX0.tNY-RncRfJrho1AKSDSp6TLdRhBQPapiSZCJFxEOu5o7wFznq7mCQhxEwnYKkoNCkMUY0OYJh8VccHQ_Bteucw";
 
         public List<Member> GetOurClan()
         {
-            string token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJjYWQxOWEyLTMyNDgtNDE2My04ZjJiLTIzNmUzMjA3NzM1MyIsImlhdCI6MTUwMTU1NzQ1Niwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzLjg0LjE4MS40NyIsIjEzLjY1LjQyLjQyIiwiMTMuNjUuNDIuMzUiXSwidHlwZSI6ImNsaWVudCJ9XX0.tNY-RncRfJrho1AKSDSp6TLdRhBQPapiSZCJFxEOu5o7wFznq7mCQhxEwnYKkoNCkMUY0OYJh8VccHQ_Bteucw";
-
             var request = new RestRequest("clans/{clanTag}/members", Method.GET);
 
             request.AddHeader("Accept", "application/json");
@@ -35,7 +34,15 @@ namespace ClashOfClans.ApiCalls
 
         public Member GetPlayerInfo(string playerTag)
         {
-            return new Member();
+            RestClient client = new RestClient("https://api.clashofclans.com/v1/");
+            var request = new RestRequest("players/{playerTag}", Method.GET);
+
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Authorization", token);
+            request.AddParameter("playerTag", playerTag, ParameterType.UrlSegment);
+
+            var response = client.Execute<Member>(request);
+            return response.Data;
         }
     }
 }
