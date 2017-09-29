54a76810c4d344953d9ba7716332218300a375ad
Donald Davis <donald.m.davis@sherwin.com>
2017-09-29 15:08:36 -0400

    refactored api

--- ClashOfClans/ApiCalls/ApiCall.cs
+++ ClashOfClans/ApiCalls/ApiCall.cs
@@ -9,17 +9,13 @@ namespace ClashOfClans.ApiCalls
     {
         Queries database = new Queries();
         RestClient client = new RestClient("https://api.clashofclans.com/v1/");
-        string token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdiM2Y4M2ZlLWRmNDAtNDVkYS05MjYzLTU0ZTdmNDRmYzFmNiIsImlhdCI6MTUwMzI4NTUyMCwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk5LjE2NC4xNzYuMzUiLCIxMy42NS40MS4xMjciLCIxMy44NC4xODEuNDciLCIxMy42NS40Mi40MiIsIjEzLjY1LjQyLjM1Il0sInR5cGUiOiJjbGllbnQifV19.VAvdlOdT0zqjAi1ZdingQ0Id-wha25EqSZyh0CKI15UYvlyM1jKcf6icB49QKiSUzFItflIt9Ih00WGnXqbjHg";
-
-        private string workToken =
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ1YTE4NWE0LTY1ODEtNDgxNi05ZTk2LWRkNTZmMDdjMGM3YSIsImlhdCI6MTUwNjY4ODg2NSwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE1Mi4xNzkuNy4yMDYiXSwidHlwZSI6ImNsaWVudCJ9XX0.mfVpcOtaRocyyIC6fhbRihDJAariTm7kbdci87YLBPWxSokc8lhu1PqDGKLEASOim8gsQ2zBsCtJnLW7-GbFFg";
 
         public void ClanApiCall()
         {
             var request = new RestRequest("clans/{clanTag}/members", Method.GET);
 
             request.AddHeader("Accept", "application/json");
-            request.AddHeader("Authorization", token);
+            request.AddHeader("Authorization", TokenStore.GetToken());
             request.AddParameter("clanTag", "#8UJGPROJ", ParameterType.UrlSegment);
             var response = client.Execute<ClanListViewModel>(request);
             var newList = new List<MemberModel>();
@@ -36,7 +32,7 @@ namespace ClashOfClans.ApiCalls
             var request = new RestRequest("players/{playerTag}", Method.GET);
 
             request.AddHeader("Accept", "application/json");
-            request.AddHeader("Authorization", token);
+            request.AddHeader("Authorization", TokenStore.GetToken());
             request.AddParameter("playerTag", playerTag, ParameterType.UrlSegment);
 
             var response = client.Execute<MemberModel>(request);
@@ -48,11 +44,23 @@ namespace ClashOfClans.ApiCalls
             var request = new RestRequest("clans/{clanTag}/currentwar", Method.GET);
             request.DateFormat = "yyyyMMddTHHmmss.FFFK";
             request.AddHeader("Accept", "application/json");
-            request.AddHeader("Authorization", token);
+            request.AddHeader("Authorization", TokenStore.GetToken());
             request.AddParameter("clanTag", "#8UJGPROJ", ParameterType.UrlSegment);
             var response = client.Execute<CurrentWar>(request);
 
             return response.Data;
+        }
+    }
+
+    internal static class TokenStore
+    {
+        private static string token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdiM2Y4M2ZlLWRmNDAtNDVkYS05MjYzLTU0ZTdmNDRmYzFmNiIsImlhdCI6MTUwMzI4NTUyMCwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk5LjE2NC4xNzYuMzUiLCIxMy42NS40MS4xMjciLCIxMy44NC4xODEuNDciLCIxMy42NS40Mi40MiIsIjEzLjY1LjQyLjM1Il0sInR5cGUiOiJjbGllbnQifV19.VAvdlOdT0zqjAi1ZdingQ0Id-wha25EqSZyh0CKI15UYvlyM1jKcf6icB49QKiSUzFItflIt9Ih00WGnXqbjHg";
+        private static string workToken =
+            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ1YTE4NWE0LTY1ODEtNDgxNi05ZTk2LWRkNTZmMDdjMGM3YSIsImlhdCI6MTUwNjY4ODg2NSwic3ViIjoiZGV2ZWxvcGVyLzhhZTlkY2MxLTY2OTctNGMwZS1jMTI1LWJkNGNkNzc0MWMwZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE1Mi4xNzkuNy4yMDYiXSwidHlwZSI6ImNsaWVudCJ9XX0.mfVpcOtaRocyyIC6fhbRihDJAariTm7kbdci87YLBPWxSokc8lhu1PqDGKLEASOim8gsQ2zBsCtJnLW7-GbFFg";
+
+        public static string GetToken()
+        {
+            return workToken;
         }
     }
 }
