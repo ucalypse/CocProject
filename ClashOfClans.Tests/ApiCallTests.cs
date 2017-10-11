c6a5ac4c1a3448c9f9236b6bbfb373c6809ad2ee
Donald Davis <donald.m.davis@sherwin.com>
2017-10-11 14:11:47 -0400

    Added test project

---
+++ ClashOfClans.Tests/ApiCallTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClashOfClans.ApiCalls;
+using ClashOfClans.Data;
+using NUnit.Framework;
+
+namespace ClashOfClans.Tests
+{
+    [TestFixture]
+    public class ApiCallTests
+    {
+        ApiCall apiCall = new ApiCall();
+        [Test]
+        public void PlayerApiCall_Returns_Member()
+        {
+            var testMember = new MemberModel
+            {
+                Name = "Dada",
+                PlayerTag = "#RPP222JV",
+                Level = 186,
+                WarStars = 720,
+                TownHallLevel = 11
+            };
+
+            var samplePlayer = apiCall.PlayerApiCall(testMember.PlayerTag);
+            Assert.That(samplePlayer.Name, Is.EqualTo("Dada"));
+        }
+    }
+}
---
+++ ClashOfClans.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClashOfClans.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ClashOfClans.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f0c0dae-d1c6-4ac4-bf91-83c99a7dbf1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
